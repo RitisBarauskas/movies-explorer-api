89e3943bf5bfd875bd4690c0d03ca9753f9dc358
Ritis <jimmyelf@gmail.com>
2021-08-31 22:15:10 +0500

    Добавлен лимитер

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { PORT = 3000, DATABASE = "mongodb://localhost:27017/moviesdb" } =
   process.env;
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { limiter } = require("./utils/limiter");
 const indexRouter = require("./routes/index");
 const { handleError } = require("./middlewares/handleError");
 const { notFoundPage } = require("./middlewares/notFoundPage");
@@ -19,19 +20,17 @@ mongoose.connect(`${DATABASE}`, {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-
 app.use(requestLogger);
-
+app.use(limiter);
 app.use("/", indexRouter);
-
 app.use(errorLogger);
 app.use(errors());
 app.get("*", notFoundPage);
 app.use(handleError);
-
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
